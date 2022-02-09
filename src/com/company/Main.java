110780eacd68add3bf5bac3a0a41793b54769659
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-09 14:05:35 -0500

    Fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,46 @@
 package com.company;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
-    static String cityNames;
-    public static void main(String[] args) {
-        System.out.println("Enter a city name");
-        cityNames = input.nextLine();
-        while (cityNames.toUpperCase() != "END"){
 
+    static HashMap<String, Double> avgTemp(HashMap<String ,Double> a){
+        System.out.println("Please enter a city name: ");
+        String city = input.nextLine();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Please enter the temp: ");
+//            double temp = input.nextDouble();
+//
+//        }
+        System.out.println("Please enter the temp:");
+        double temp1 = input.nextDouble();
+        System.out.println("Please enter the temp:");
+        double temp2 = input.nextDouble();
+
+        double total = temp1 + temp2;
+        double avg = total/5;
+        System.out.println(avg);
+
+        return a;
+    }
+
+
+    public static void main(String[] args) {
+        Map<String, Double> cities = new TreeMap<String, Double>();
+//        ArrayList<String> city = new ArrayList<>();
+
+        System.out.println("Enter a city name");
+        var cityNames = input.nextLine();
+        cities.put(cityNames, 68.0);
+        System.out.println(cities);
+
+        while (!(cityNames.equalsIgnoreCase("END"))){
             System.out.println("Enter a city name");
             cityNames = input.nextLine();
-
-
         }
+        avgTemp();
+
     }
+
 }
