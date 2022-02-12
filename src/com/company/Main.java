7f792a87159a39f1d5cacd0ccf42a87eb40c5bee
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-12 04:10:11 -0500

    Update Main.java
    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,59 @@
+// Leke Obayemi
+// 2/12/22
+// Exercise 4
+
+
 package com.company;
 
 import java.util.*;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
-
-    static HashMap<String, Double> avgTemp(HashMap<String ,Double> a){
-        System.out.println("Please enter a city name: ");
-        String city = input.nextLine();
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println("Please enter the temp: ");
-//            double temp = input.nextDouble();
-//
-//        }
-        System.out.println("Please enter the temp:");
-        double temp1 = input.nextDouble();
-        System.out.println("Please enter the temp:");
-        double temp2 = input.nextDouble();
-
-        double total = temp1 + temp2;
-        double avg = total/5;
-        System.out.println(avg);
-
-        return a;
-    }
-
-
-    public static void main(String[] args) {
-        Map<String, Double> cities = new TreeMap<String, Double>();
-//        ArrayList<String> city = new ArrayList<>();
-
-        System.out.println("Enter a city name");
-        var cityNames = input.nextLine();
-        cities.put(cityNames, 68.0);
-        System.out.println(cities);
-
-        while (!(cityNames.equalsIgnoreCase("END"))){
-            System.out.println("Enter a city name");
-            cityNames = input.nextLine();
+    static HashMap<String, Double> cities = new HashMap<String, Double>();
+    static String cityNames = "";
+     public static void main(String[] args) {
+        
+        while (!(cityNames.equalsIgnoreCase("END"))){       //always asks for city name and daily temps until user enters "end"
+            prompt();       //prompts user for city name
+            getAvgTemp();   //prompt user for temps for the next 5 days and gives an average
         }
-        avgTemp();
-
+        results();      //prints out all reults in HashMap
     }
 
+
+    static void prompt(){
+        System.out.println("Enter a city name");
+        input.nextLine();   //nextLine method is bugged, calling a temporary nextLine before calling a real nextLine
+        String cityNames1 = input.nextLine();
+        cityNames = cityNames1;
+    }
+
+    static void getAvgTemp(){
+        ArrayList<Double> temp = new ArrayList<>();
+        double sum = 0;
+        double avgTemp = 0;
+
+        if(!(cityNames.equalsIgnoreCase("END"))){       //will ask for daily temps as long as if the user doessn't enter "end"
+            for (int i = 0; i < 5 ; i++) {
+                System.out.println("Enter in a temperature");    
+                temp.add(input.nextDouble());
+
+            }
+
+            for (int i = 0; i < temp.size() ; i++) {        //sums up doubles in arrayList and divides by 5
+                sum = sum + temp.get(i);
+                avgTemp = sum/5;
+                
+            }
+
+            cities.put(cityNames, avgTemp);
+        }  
+    }
+
+    static void results(){
+        System.out.println("\nCities and Average Temperatures:\n");
+        for (String i : cities.keySet()) {
+            System.out.println("Cities: " + i + "\nAverage Temperature: " + cities.get(i));     //prints both city names and average temps
+        }
+    }
 }
